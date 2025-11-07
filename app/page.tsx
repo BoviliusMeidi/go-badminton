0ba3791456340080673da157af9154f5caddbdc0
BoviliusMeidi <boviliusbm@gmail.com>
2025-11-07 19:33:16 +0700

    feat(pages) : add Hero and ReviewCourt to the HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
-import Image from "next/image";
+/**
+ * @fileoverview
+ * This file contains the main Home page component for the application.
+ * It serves as the root entry point (`/`) and assembles the different
+ * sections of the homepage.
+ */
 
-export default function Home() {
+"use client";
+
+import { JSX } from "react";
+import Hero from "./_components/Hero";
+import ReviewCourt from "./_components/ReviewCourt";
+
+export default function Home(): JSX.Element {
   return (
     <>
+      <Hero />
+      <ReviewCourt />
     </>
   );
 }
